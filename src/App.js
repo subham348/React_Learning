c337eefbc58e8f13508c1006aab5b9b5fbfc1c18
subham348 <subhammahato348@gmail.com>
2021-09-07 01:53:16 +0530

    Studied about props, state, setState and Event Handling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
 import Hello from "./components/Hello"
+import Message from './components/Message';
+import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 function App() {
   return (
@@ -15,6 +19,10 @@ function App() {
       <Greet name="Tarun" heroName="Hanuman"/>
       <Welcome />
       <Hello />
+      <Message />
+      <Counter />
+      <FunctionClick />
+      <ClassClick />
     </div>
   );
 }
