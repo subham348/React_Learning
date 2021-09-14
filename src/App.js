d3c1bff8eacbfad7cad558b1a4355930abdaba90
subham348 <subhammahato348@gmail.com>
2021-09-14 22:59:55 +0530

    Studied about Styling and CSS Basics and Form Handling

--- src/App.js
+++ src/App.js
@@ -12,10 +12,17 @@ import EventBind from "./components/EventBind";
 import ParentComponent from "./components/ParentComponent";
 import UserGreeting from "./components/UserGreeting";
 import NameList from "./components/NameList";
+import Stylesheets from "./components/Stylesheets";
+import Inline from "./components/Inline";
+import "./appStyles.css";
+import styles from "./appStyles.module.css";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
+      <Form />
+
       <Greet name="Sourav" heroName="Batman">
         <p>This is children props</p>
       </Greet>
@@ -31,6 +38,10 @@ function App() {
       <ParentComponent />
       <UserGreeting />
       <NameList />
+      <Stylesheets primary={true} />
+      <Inline />
+      <h1 className="error">Error</h1>
+      <h1 className={styles.success}>Success</h1>
     </div>
   );
 }
